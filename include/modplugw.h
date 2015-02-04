a6d1dec3f08ce866214e608cb55f44cc400da904
Lubomir I. Ivanov <neolit123@gmail.com>
2015-02-04 13:55:34 +0200

    add version info and CHANGELOG
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- include/modplugw.h
+++ include/modplugw.h
@@ -11,6 +11,10 @@
 #ifndef MODPLUGW_H
 #define MODPLUGW_H
 
+#define MODPLUGW_VERSION "0.1"
+#define MODPLUGW_VERSION_MAJOR 0
+#define MODPLUGW_VERSION_MINOR 1
+
 #ifdef __cplusplus
 extern "C" {
 #endif
