53e1d1399f52dec20ec4f0752c516dbaa65a9d43
Lubomir I. Ivanov <neolit123@gmail.com>
2015-02-09 22:59:55 +0200

    VERSION: bump to 0.2.1
    Signed-off-by: Lubomir I. Ivanov <neolit123@gmail.com>

--- include/modplugw.h
+++ include/modplugw.h
@@ -17,10 +17,10 @@ extern "C" {
 
 #include <modplug.h>
 
-#define MODPLUGW_VERSION "0.2.0"
+#define MODPLUGW_VERSION "0.2.1"
 #define MODPLUGW_VERSION_MAJOR 0
 #define MODPLUGW_VERSION_MINOR 2
-#define MODPLUGW_VERSION_PATCH 0
+#define MODPLUGW_VERSION_PATCH 1
 
 #ifdef MODPLUGW_DYNAMIC
 #	define MODPLUGW_EXPORT __declspec(dllexport)
